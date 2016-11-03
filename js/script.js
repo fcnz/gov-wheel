c38960b72a340a8934333bd0fc45b3419692bc02
Fraser Croad <fraser.croad@gmail.com>
2016-11-04 01:01:18 +1300

    Make rings snap into place, make description show up

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-﻿var pi = Math.PI;
+﻿const pi = Math.PI;
 
 //creating the SVG element
-var width = $('.wheel').width();
-var height = $('.wheel').height();
-var radius = Math.min(width, height) / 2;
-var curAngle = 0;
+const width = $('.wheel').width();
+const height = $('.wheel').height();
+const radius = Math.min(width, height) / 2;
+const curAngle = 0;
 
-var color = d3.scaleOrdinal()
-.range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56', '#d0743c', '#ff8c00']);
+const color = d3.scaleOrdinal()
+.range(['#ff8c00', '#98abc5', '#a05d56', '#8a89a6', '#7b6888', '#6b486b']);
 
-var svg = d3.select('.wheel').append('svg')
+const svg = d3.select('.wheel').append('svg')
 .attr('width', width)
 .attr('height', height)
 .append('g')
@@ -17,37 +17,32 @@ var svg = d3.select('.wheel').append('svg')
 
 //Here is the for loop that creates each ring. 'i' is the number of rings.
 data.forEach(function(ringData, i) {
+  const centreX = width / 2 + 15; // Trial and error
+  const centreY = height / 2 + 135; // Add distance from edge of page
+  const increment = 360 / ringData.length;
+  const outer = radius - 10 - (radius - 50) / data.length * i;
+  const inner = radius - 10 - (radius - 50) / data.length * (i + 1);
 
-  var svg = d3.select('svg')
+  const svg = d3.select('svg')
   .attr('width', width)
   .attr('height', height)
   .append('g')
-  .attr('transform', `translate(${width / 2},${height / 2})`);
+  .attr('transform', `translate(${width / 2},${height / 2})  rotate(${increment / 2})`);
 
   //setting the outer and inner radius of each ring:
-  if (i == 0) {
-    var outer = radius - 10;
-    var inner = radius - 45 * (i + 1);
-    var arc = d3.arc()
-    .outerRadius(outer)
-    .innerRadius(inner);
-  } else {
-    var outer = radius - 45 * i;
-    var inner = radius - 45 * (i + 1);
-    var arc = d3.arc()
-    .outerRadius(outer)
-    .innerRadius(inner);
-  }
+  const arc = d3.arc()
+  .outerRadius(outer)
+  .innerRadius(inner);
 
   // setting up for the sections of each ring
-  var pie = d3.pie()
+  const pie = d3.pie()
   .sort(null)
   .value(function(d) {
     return 1;
   });
 
   // mapping the heading from ringData to each section
-  var g = svg.selectAll('.arc')
+  const g = svg.selectAll('.arc')
   .data(pie(ringData))
   .enter().append('g')
   .attr('class', 'arc');
@@ -55,29 +50,29 @@ data.forEach(function(ringData, i) {
   g.append('path')
   .attr('d', arc)
   .style('fill', function(d) {
-    return color(d.data.heading);
+    return color(i);
   });
 
   ringData.forEach(function(slice, j) {
-    var textRadius = (outer + inner) / 2;
+    const textRadius = (outer + inner) / 2;
 
-    var theta1 = (-pi / 2) + ((2 * pi) / ringData.length) * j;
-    var xStart = textRadius * Math.cos(theta1);
-    var yStart = textRadius * Math.sin(theta1);
+    const theta1 = (-pi / 2) + ((2 * pi) / ringData.length) * j;
+    const xStart = textRadius * Math.cos(theta1);
+    const yStart = textRadius * Math.sin(theta1);
 
-    var theta2 = (-pi / 2) + ((2 * pi) / ringData.length) * (j + 1);
-    var xEnd = textRadius * Math.cos(theta2);
-    var yEnd = textRadius * Math.sin(theta2);
+    const theta2 = (-pi / 2) + ((2 * pi) / ringData.length) * (j + 1);
+    const xEnd = textRadius * Math.cos(theta2);
+    const yEnd = textRadius * Math.sin(theta2);
 
-    var pathVar = `M ${xStart} ${yStart} A ${textRadius} ${textRadius} 0 0 1 ${xEnd} ${yEnd}`;
+    const pathconst = `M ${xStart} ${yStart} A ${textRadius} ${textRadius} 0 0 1 ${xEnd} ${yEnd}`;
 
     svg.append('defs').append('path')
     .attr('id', 'curve' + i + j)
-    .attr('d', pathVar)
+    .attr('d', pathconst)
 
-    var pathEl   = d3.select('#curve' + i + j).node();
-    var pathLength = pathEl.getTotalLength()
-    var midpoint = pathEl.getPointAtLength(pathEl.getTotalLength()/2);
+    const pathEl   = d3.select('#curve' + i + j).node();
+    const pathLength = pathEl.getTotalLength()
+    const midpoint = pathEl.getPointAtLength(pathEl.getTotalLength()/2);
 
     svg.append('text')
     .attr('x', Math.abs(pathLength/2))
@@ -94,51 +89,53 @@ data.forEach(function(ringData, i) {
     g.append('use')
     .attr('class', 'curve-line')
     .attr('xlink:href', '#curve');
+
+    d3.selectAll('.curve-text').on('click', function(a) {
+      console.log(a)
+    })
+
+    g.on('click', function(element) {
+      const { heading, description } = element.data;
+      $('.title').text(heading);
+      $('.description').text(description);
+    })
   });
 
   //Here starts the code for the mouse events
-  var interval = null;
-  var isDown = false;
-  var lastX = width / 2;
-  var lastY = height / 2;
-  var curAngle = 0;
-  var finishAngle = 0;
-  var angleDeg = 0;
-  var testbool = true;
+  let endAngle = increment / 2;
+  let startAngle;
 
   window.onload = function() {
     document.onmousedown = function() {return false;} // mozilla
   }
 
   svg.on('mousedown', function() {
-    d3.select(window);
-    isDown = true;
-    var thisX = d3.event.x - lastX;
-    var thisY = d3.event.y - lastY;
-    curAngle = Math.atan2(lastY - d3.event.y, lastX - d3.event.x) // * 180 / pi) ;
-    if (curAngle < 0) curAngle += 2 * pi;
-    curAngle = curAngle * 180 / pi;
+    startAngle = Math.atan2(d3.event.y - centreY, d3.event.x - centreX) * 180 / pi;
 
+    // Set move and up listeners on entire window
     d3.select(window).on('mousemove', mousemove);
-
     d3.select(window).on('mouseup', mouseup);
 
     function mousemove() {
-      if (isDown) {
-        var thisX = d3.event.x - lastX;
-        var thisY = d3.event.y - lastY;
-        angleDeg = Math.atan2(lastY - d3.event.y, lastX - d3.event.x) // * 180 / pi) - curAngle
-        if (angleDeg < 0) angleDeg += 2 * pi;
-        angleDeg = angleDeg * 180 / pi;
-        angleDeg = angleDeg - curAngle + finishAngle;
-        if (angleDeg < 0) angleDeg += 360;
-        svg.attr('transform', `translate(${width / 2},${height / 2}) rotate(${angleDeg},0,0)`);
-      }
+      const thisAngle = Math.atan2(d3.event.y - centreY, d3.event.x - centreX) * 180 / pi;
+      let moveAngle = thisAngle - startAngle + endAngle;
+      if (moveAngle < 0) moveAngle += 360;
+      svg.attr('transform', `translate(${width / 2},${height / 2}) rotate(${moveAngle},0,0)`);  
     }
 
     function mouseup() {
-      finishAngle = angleDeg;
-      isDown = false;
+      const thisAngle = Math.atan2(d3.event.y - centreY, d3.event.x - centreX) * 180 / pi;
+      let moveAngle = thisAngle - startAngle + endAngle;
+      if (moveAngle < 0) moveAngle += 360;
+      // Check which increment is nearest and translate to that instead of doing it smoothly
+      moveAngle = Math.round((moveAngle + increment / 2) / increment) * increment - increment / 2;
+      svg.attr('transform', `translate(${width / 2},${height / 2}) rotate(${moveAngle},0,0)`);
+      endAngle = moveAngle;
+
+      console.log(((endAngle - increment / 2) / increment) % ringData.length); // Says which segment it pointing up
+
+      d3.select(window).on('mousemove', function() {});
+      d3.select(window).on('mouseup', function() {});
     }
   });
 });
