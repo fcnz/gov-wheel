1fb8187643d20b2aaf06e773bf9bbb3e273d7121
Fraser Croad <fraser.croad@gmail.com>
2016-11-09 08:54:47 +1300

    Use real data in fixtures

--- js/fixtures.js
+++ js/fixtures.js
@@ -1,127 +1,164 @@
 var data = [
   [
     {
-      "heading": "AAAAAAAAAAAAAAAAAAAA",
-      "description": 2704659
+      heading: 'Federated (Club)',
+      description: 'NSO voting members are the local clubs, with regional entities or committees potentially in existence as service centres (Sport NZ, n.d.).'
     },
     {
-      "heading": "BBBmy name is Megan BBB",
-      "description": 4499890
-    },
-    {
-      "heading": "CCCCCCCCCCCC",
-      "description": 2159981
-    },
-    {
-      "heading": "DDDDDDDDDDDDDD",
-      "description": 3853788
-    },
-    {
-      "heading": "EEEEEEEEEEEEEE",
-      "description": 141065
-    },
-    {
-      "heading": "FFFFFFFFFFFFF",
-      "description": 8819342
-    },
-    {
-      "heading": "GGGGGGGGGG",
-      "description": 612463
-    }
-
-  ],
-  [
-    {
-      "heading": "A",
-      "description": 2704659
-    },
-    {
-      "heading": "BBBBBBBBBBBBB",
-      "description": 4499890
-    },
-    {
-      "heading": "C",
-      "description": 2159981
-    },
-    {
-      "heading": "Test",
-      "description": 3853788
-    },
-    {
-      "heading": "E",
-      "description": 141065
-    },
-    {
-      "heading": "Here is a sentence",
-      "description": 8819342
-    },
-    {
-      "heading": "G",
-      "description": 612463
-    },
-    {
-      "heading": "H",
-      "description": 1000000
+      heading: 'Unitary',
+      description: 'NSO voting members are the individuals, for example, Triathlon NZ, as an events-based organisation, has a structure closely aligned with the unitary model (Sport NZ, n.d.).'
     }
   ],
   [
     {
-      "heading": "A",
-      "description": 2704659
+      heading: 'Director Selection\n(Delegate)',
+      description: 'Board composition is determined by the members and/or interest groups exercising their right to delegate someone to represent them on the board (Delegation, n.d.).'
     },
     {
-      "heading": "B",
-      "description": 4499890
+      heading: 'Director Selection\n(Elected)',
+      description: 'Board composition is detemoined by the members, collectively, conduct a formal and organized choice by vote (Election, n.d.).'
     },
     {
-      "heading": "CCCCCCCCCCCCCCCCCCC",
-      "description": 2159981
+      heading: 'Director Selection\n(Appointed)',
+      description: 'Board composition is determined by an authorised person/entity assigning the position on the board to someone (Appointment, n.d.).'
     },
     {
-      "heading": "D",
-      "description": 3853788
+      heading: 'Director Selection\n(Co-Opted)',
+      description: 'Co-option is the process of appointing someone to the board by invitation of the existing members [of the board] (Co-option, n.d.).'
     },
     {
-      "heading": "E",
-      "description": 141065
-    },
-    {
-      "heading": "F",
-      "description": 8819342
-    },
-    {
-      "heading": "G",
-      "description": 612463
-    },
-    {
-      "heading": "H",
-      "description": 1000000
+      heading: 'Director Selection\n(Mixed Model)',
+      description: 'Board composition is determined by a mixture of delegation, election, appointment and/or co-option.'
     }
   ],
   [
     {
-      "heading": "A",
-      "description": 2704659
+      heading: 'Objective - Oversight',
+      description: 'Encompasses the conformance objective (e.g., monitoring and accountability functions) of the board in achieving its overall aim of good governance (Ferkins & Kilmister, 2012).'
     },
     {
-      "heading": "B",
-      "description": 4499890
+      heading: 'Objective - Foresight',
+      description: 'Encompasses the performance objective (e.g., strategic planning function) of the board in achieving its overall aim of good governance (Ferkins & Kilmister, 2012).'
     },
     {
-      "heading": "C",
-      "description": 2159981
-    },
-    {
-      "heading": "Yayyyyyy!",
-      "description": 3853788
-    },
-    {
-      "heading": "E",
-      "description": 141065
-    },
-    {
-      "heading": "F",
-      "description": 8819342
+      heading: 'Objective - Insight',
+      description: 'Encompasses the ??? objective (e.g., philosphicial, ethical and critical thinking to predict, understand and resolve complex situations) of the board in achieving its overall aim of good governance (Ferkins & Kilmister, 2012).'
     }
   ],
+  [
+    {
+      heading: 'Function - Financial',
+      description: ''
+    },
+    {
+      heading: 'Function - CEO Relationship',
+      description: ''
+    },
+    {
+      heading: 'Function - Stakeholder Relationships',
+      description: ''
+    },
+    {
+      heading: 'Function - ???',
+      description: ''
+    }
+  ],
+  [
+    {
+      heading: 'Tasks - Revenue Generation',
+      description: ''
+    },
+    {
+      heading: 'Tasks - Audit Related',
+      description: ''
+    },
+    {
+      heading: 'Tasks - ??? (could be a lot more)',
+      description: ''
+    }
+  ],
+  [
+    {
+      heading: 'Theory - Agency',
+      description: ''
+    },
+    {
+      heading: 'Theory - Resource Dependency',
+      description: ''
+    },
+    {
+      heading: 'Theory - Stewardship',
+      description: ''
+    },
+    {
+      heading: 'Theory - Stakeholder',
+      description: ''
+    },
+    {
+      heading: 'Theory - Stakeowner',
+      description: ''
+    },
+    {
+      heading: 'Theory - Democratic',
+      description: ''
+    },
+    {
+      heading: 'Theory - Managerial Hegemony',
+      description: ''
+    },
+    {
+      heading: 'Theory - Institutional',
+      description: ''
+    },
+    {
+      heading: 'Theory - ???',
+      description: ''
+    }
+  ],
+  [
+    {
+      heading: 'Theory - ???',
+      description: ''
+    },
+    {
+      heading: 'Goverance - Compliance Model',
+      description: 'The main function of the board is to control managers... to ensure [management] acts in the shareholders best interests (Cornforth, 2004, p. 14). '
+    },
+    {
+      heading: 'Governace - Partnership Model',
+      description: 'The main function of the board is ... to improve organisational performance...to work with management to improve strategy and add value to top decisions (Cornforth, 2004, p. 15).'
+    },
+    {
+      heading: 'Goverance - Democratic Model',
+      description: 'The main function of the board is to resolve or choose between the interests of different groups and set the overall policy of the organisation, which can then be implemented by staff (Cornfirth, 2004, p. 14).'
+    },
+    {
+      heading: 'Goverance - Stakeholder Model',
+      description: 'The main function of the board is political,  negotiating and resolving the potentially conflicting interests of different stakeholder groups in order to determine the objectives of the organisation and set policy (Cornforth, 2004, p. 17).'
+    },
+    {
+      heading: 'Governance - Co-optation Model',
+      description: 'The main functions of the board are to maintain good relations with key external stakeholders in order to ensure the flow of resources..., and to help the organsition respond to external change (Cornforth, 2004, pp. 16, 17).'
+    },
+    {
+      heading: 'Governance - \'Rubber stamp\' Model',
+      description: 'The board\'s function is essentially symbolic to give legitimacy to managerial actions (Cornforth, 2004, p. 18).'
+    },
+    {
+      heading: 'Governance - ??? ',
+      description: ''
+    },
+    {
+      heading: 'Governance - ???',
+      description: ''
+    },
+    {
+      heading: 'Governance - Contingency Model',
+      description: ''
+    },
+    {
+      heading: 'Governance - Paradox Approach',
+      description: 'The board is required to manage the complex and paradoxical relationship between boards and senior managers... through [ongoing] explicit discussion and negotiation over roles and responsibilities (Cornforth, 2004, p. 26). '
+    }
+  ]
 ];
